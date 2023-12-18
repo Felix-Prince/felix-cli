de5d28f0fcb78f8312aa9dccb0cf7f8d10579499
Felix.Price <zhupengfei1995@gmail.com>
2023-12-18 21:47:18 +0800

    feat: 完善 create 指令

--- index.js
+++ index.js
@@ -6,21 +6,18 @@ const fs = require("fs")
 program
     .command("create <filename>")
     .description("Create a new file")
-    .action((filename) => {
-        fs.writeFileSync(filename, "")
-        console.log(`File '${filename}' created successfully.`)
-    })
-
-program
-    .command("delete <filename>")
-    .description("Delete a file")
-    .action((filename) => {
-        try {
-            fs.unlinkSync(filename)
-            console.log(`File '${filename}' deleted successfully.`)
-        } catch (error) {
-            console.error(`Error deleting file: ${error.message}`)
+    .option("-c, --content <content>", "文件内容", "这是内容这是内容这是内容")
+    .option("-t, --title <title>", "这是标题", "This is Title")
+    .action(async (filename, option) => {
+        console.log(option)
+        if (option.title) {
+            fs.writeFileSync(filename, `# ${option.title}`)
         }
+        if (option.content) {
+            const content = await fs.readFileSync(filename)
+            fs.writeFileSync(filename, content + "\n" + option.content)
+        }
+        console.log(`File '${filename}' created successfully.`)
     })
 
 program.parse(process.argv)
