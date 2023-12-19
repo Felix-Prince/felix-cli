bf0786543b8659ed5fdea0f6f2005afd0686021b
Felix.Price <zhupengfei1995@gmail.com>
2023-12-19 21:44:07 +0800

    feat: 尝试 ejs 的模板和指令的结合

---
+++ Demo.js
@@ -0,0 +1,13 @@
+// templates/component.ejs
+import React from 'react';
+
+const Demo = () => {
+  return (
+    <div>
+      <h1>Demo</h1>
+      {/* Your component logic goes here */}
+    </div>
+  );
+};
+
+export default Demo;
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const { program } = require("commander")
 const fs = require("fs")
+const path = require('path');
+const ejs = require('ejs');
 
 program
     .command("create <filename>")
@@ -9,15 +11,33 @@ program
     .option("-c, --content <content>", "文件内容", "这是内容这是内容这是内容")
     .option("-t, --title <title>", "这是标题", "This is Title")
     .action(async (filename, option) => {
-        console.log(option)
-        if (option.title) {
-            fs.writeFileSync(filename, `# ${option.title}`)
-        }
-        if (option.content) {
-            const content = await fs.readFileSync(filename)
-            fs.writeFileSync(filename, content + "\n" + option.content)
-        }
-        console.log(`File '${filename}' created successfully.`)
+        const templatePath = path.join(__dirname, 'templates', 'component.ejs');
+        const outputPath = path.join(process.cwd(), filename + '.js');
+        // 读取 EJS 模板文件
+        const templateContent = fs.readFileSync(templatePath, 'utf-8');
+
+        // 编译 EJS 模板
+        const template = ejs.compile(templateContent);
+
+        // 定义模板数据
+        const templateData = { componentName: filename };
+
+        // 渲染模板并写入文件
+        const result = template(templateData);
+        fs.writeFileSync(outputPath, result);
+
+        console.log(`Component '${filename}' generated successfully at: ${outputPath}`);
+
+
+        // console.log(option)
+        // if (option.title) {
+        //     fs.writeFileSync(filename, `# ${option.title}`)
+        // }
+        // if (option.content) {
+        //     const content = await fs.readFileSync(filename)
+        //     fs.writeFileSync(filename, content + "\n" + option.content)
+        // }
+        // console.log(`File '${filename}' created successfully.`)
     })
 
 program.parse(process.argv)
